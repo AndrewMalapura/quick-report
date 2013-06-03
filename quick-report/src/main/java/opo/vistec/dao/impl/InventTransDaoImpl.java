df8d11f09c6b3c56bc1d02523749391753b88f65
AndrewMalapura <malapura@mail.ru>
2013-06-03 16:52:31 +0300

    work4

---
+++ quick-report/src/main/java/opo/vistec/dao/impl/InventTransDaoImpl.java
@@ -0,0 +1,7 @@
+package opo.vistec.dao.impl;
+
+import opo.vistec.dao.InventTransDAO;
+
+public class InventTransDaoImpl implements InventTransDAO {
+
+}
---
+++ quick-report/src/main/java/opo/vistec/entity/impl/InventTransBoImpl.java
@@ -0,0 +1,7 @@
+package opo.vistec.entity.impl;
+
+import opo.vistec.entity.InventTransBo;
+
+public class InventTransBoImpl implements InventTransBo {
+
+}
